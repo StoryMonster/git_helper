4a763e0034990ed48182d222d9f38dbc29311262
dochen <StoryMonster@aliyun.com>
2018-07-10 11:01:51 +0800

    Add new function: search commit id
    1. fix the search commit id bug
    2. rename function search as search_content_in_commit_info
    3. add function search_commit_id
    4. redefine the previous commit as the merge date ealier current commit,
    next commit is later than current commit

--- src/commitSearcher.py
+++ src/commitSearcher.py
@@ -1,4 +1,4 @@
-import queue
+
 
 def print_commit(commit_info, prev_commit_id, next_commit_id):
     print("\033[40;33m{}\033[0m".format(commit_info[0]))
@@ -11,7 +11,7 @@ def print_commit(commit_info, prev_commit_id, next_commit_id):
     print("")
 
 
-def search(keyword):
+def search_content_in_commit_info(keyword):
     keyword = keyword.lower()
     act_findout, commit_info = False, list()
     prev_commit_id = next_commit_id = ''
@@ -29,9 +29,36 @@ def search(keyword):
             commit_counter += 1
             if commit_counter == 1: continue
             if act_findout:
-                next_commit_id = line.split()[-1]
+                prev_commit_id = line.split()[-1]
                 print_commit(commit_info[0:-1], prev_commit_id, next_commit_id)
-                next_commit_id = ''
+                prev_commit_id = ''
                 act_findout = False
-            prev_commit_id = commit_info[0].split()[-1]
+            next_commit_id = commit_info[0].split()[-1]
             commit_info = [line]
+
+            
+def search_commit_id(commit_id):
+    act_findout, commit_info = False, list()
+    prev_commit_id = next_commit_id = ''
+    while True:
+        try:
+            line = input().lower()
+        except EOFError:
+            if act_findout:
+                print_commit(commit_info, prev_commit_id, next_commit_id)
+            break
+        if line.find("commit ") != -1 and len(line) == 47:
+            current_commit_id = line.split()[-1]
+            if act_findout:
+                prev_commit_id = current_commit_id
+                print_commit(commit_info, prev_commit_id, next_commit_id)
+                break
+            else:
+                if current_commit_id.find(commit_id) == 0:
+                    act_findout = True
+                    commit_info.append(line)
+                else:
+                    next_commit_id = current_commit_id
+        elif act_findout:
+            commit_info.append(line)
+    
--- src/gitHelper.py
+++ src/gitHelper.py
@@ -1,5 +1,6 @@
 import sys
-from commitSearcher import search
+import re
+from commitSearcher import search_content_in_commit_info, search_commit_id
 from conflictSolver import show_conflicts, apply_solutions
 from statusScanner import collect_modifications
 from statistics import git_log_statistic
@@ -10,8 +11,14 @@ if __name__ == '__main__':
     argv, argc = sys.argv, len(sys.argv)
     assert argc >= 2, "githelper need at least 2 options"
     if argv[1] == 'find':
-        assert argc == 3, "what to find?"
-        search(argv[2])
+        if argv[2] == "-i":
+            assert argc == 4, "input the commit id"
+            assert len(argv[3]) >= 7, "the length of commit id at least 7"
+            assert re.match(r"[a-f0-9]+", argv[3]), "incorrect commit id"
+            search_commit_id(argv[3])
+        else:
+            assert argc == 3, "what to find?"
+            search_content_in_commit_info(argv[2])
     elif argv[1] == 'applyconflict':
         assert argc == 2, "extra options are found"
         apply_solutions()
