902bc5ea0598b0209c4395520a013fd341077ab4
dochen <StoryMonster@aliyun.com>
2018-07-12 14:54:21 +0800

    do optimization of status check

--- src/statusScanner.py
+++ src/statusScanner.py
@@ -1,48 +1,27 @@
 import os
+import re
 
 
-def print_operators(files_to_add, files_to_remove, untracked_files):
-    print('\n\n\n\n')
-    for file in files_to_add:
-        print("git add {}".format(file))
-    for file in files_to_remove:
-        print("git rm {}".format(file))
-    print("\n===Below are untracked files===")
-    if len(untracked_files) == 0:
-        print('<None>\n')
-        return
-    for file in untracked_files:
-        print("git add {}".format(file))
-    print('\n\n')
+def match(str):
+    patterns_with_types = ((r"^\s+modified:\s+(\S+)$", "modify"),
+                           (r"^\s+deleted:\s+(\S+)$", "delete"),
+                           (r"^\s+(\S+)$", "untrack"))
+    for pattern_with_type in patterns_with_types:
+        matched = re.match(pattern_with_type[0], str)
+        if matched:
+            return matched.group(1), pattern_with_type[1]
+    return "", "useless"
 
 
-def get_file_from_line(line):
-    lst = line.split()
-    for item in lst:
-        if os.path.exists(item):
-            return item
-
-            
 def collect_modifications():
-    act_tracked = True
-    files_to_add = list()
-    files_to_remove = list()
-    untracked_files = list()
-    while True:
-        try:
-            line = input()
-            print(line)
-        except EOFError:
-            print_operators(files_to_add, files_to_remove, untracked_files)
-            break
-        if act_tracked:
-            if line.find("modified:") != -1:
-                files_to_add.append(get_file_from_line(line))
-            elif line.find("deleted:") != -1:
-                files_to_remove.append(get_file_from_line(line))
-            elif line.find("Untracked files:") != -1:
-                act_tracked = False
+    git_status_res = os.popen("git status").readlines()
+    for line in git_status_res:
+        matched_file, matched_type = match(line)
+        if matched_type == "modify":
+            print("\033[40;31mgit add {}\033[0m".format(matched_file))
+        elif matched_type == "delete":
+            print("\033[40;31mgit rm {}\033[0m".format(matched_file))
+        elif matched_type == "untrack" and os.path.exists(matched_file):
+            print("\033[40;31mgit add {}\033[0m".format(matched_file))
         else:
-            file = get_file_from_line(line)
-            if file:
-                untracked_files.append(file);
+            print(line.strip('\n'))
